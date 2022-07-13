dae18bbee293c6612e4f6486b73750d51d2bfc9c
jaykumarjp <42172283+jaykumarjp@users.noreply.github.com>
2022-07-13 16:44:38 +0530

    Update code.sol

--- code.sol
+++ code.sol
@@ -1,1 +1,17 @@
-##
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity = 0.8.7;
+contract Supply{
+    int public totalSupply;
+    constructor() {
+        totalSupply = 1000;
+    }
+    function getBalance() public view returns(int supply){
+        return totalSupply;
+    }
+    function increaseSupply(int amount) public {
+        totalSupply = totalSupply+amount;
+    }
+    function decreaseSupply(int amount) public {
+        totalSupply = totalSupply-amount;
+    }
+}
